b990bb0bc555081148dea3e3ce8931b8cfcc7cfe
sina2104 <sina.ah.1.2@gmail.com>
2023-12-26 02:49:59 +0300

    added task

---
+++ model.py
@@ -0,0 +1,22 @@
+﻿import random
+from transformers import AutoTokenizer, GPT2LMHeadModel, pipeline
+
+model_name = "sberbank-ai/rugpt3small_based_on_gpt2"
+model = GPT2LMHeadModel.from_pretrained(model_name).to("cpu")
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+generation = pipeline("text-generation", model=model, tokenizer=tokenizer, device="cpu")
+
+config = {
+        "max_length": random.randint(200, 350),
+        "temperature": 1.1,
+        "top_p": 2.0,
+        "num_beams": 10,
+        "repetition_penalty": 1.5,
+        "num_return_sequences": 9,
+        "no_repeat_ngram_size": 2,
+        "do_sample": True,
+        }
+
+def generate_res(response_) -> str:
+    output = generation(response_, **config)[0]["generated_text"]
+    return output
---
+++ run.py
@@ -0,0 +1,23 @@
+﻿import streamlit as st
+from model import generate_res
+
+
+st.set_page_config(
+    page_title='Генерация КНИР',
+    initial_sidebar_state="expanded",
+)
+st.title('Генерация для КНИР')
+
+
+text_ = st.text_area('Введите запрос:', '', height=25, key='text')
+
+button_ = st.button("Сгенерировать:")
+
+if button_:
+    container = st.container()
+    
+    placeholder = container.empty()
+
+    response_ = generate_res(text_)
+
+    placeholder.info(response_)
